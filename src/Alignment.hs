9a58ca2612a758acced4dc3a1a9ff3999cbe0028
Tomasz Kępa <misterecco@gmail.com>
2019-03-18 22:06:57 +0100

    Implement SMP -- not tested

--- src/Alignment.hs
+++ src/Alignment.hs
@@ -1,7 +1,7 @@
 module Alignment where
 
 import Control.Monad.State
-import Data.Map as M ( Map, empty, insertWith )
+import qualified Data.Map as M ( Map, empty, insertWith, insert, (!), size, keys, elemAt, null, delete )
 import Data.List ( nub, (\\), sort )
 
 import Parser
@@ -12,6 +12,7 @@ import Debug.Trace
 data EventDiff = EDLeft CodeEvent | EDCommon CodeEvent | EDRight CodeEvent
     deriving (Eq, Ord)
 type TraceDiff = [EventDiff]
+type Score = Integer
 
 instance Show EventDiff where
     show (EDLeft (CE eventType loc _)) =
@@ -21,7 +22,7 @@ instance Show EventDiff where
     show (EDRight (CE eventType loc _)) =
         unlines ["                              RIGHT: ", "Event: " ++ show eventType, "Loc: " ++ show loc]
 
-alignTraces :: CallTrace -> CallTrace -> (Integer, TraceDiff)
+alignTraces :: CallTrace -> CallTrace -> (Score, TraceDiff)
 alignTraces leftTrace rightTrace = 
     if null leftTrace || null rightTrace || (head leftTrace) /= (head rightTrace)
         then (-1, [])
@@ -42,28 +43,111 @@ alignTraces leftTrace rightTrace =
 
 removeDuplicateTraces :: [CallTrace] -> [CallTrace] -> ([CallTrace], [CallTrace])
 removeDuplicateTraces leftTraces rightTraces = do
-    let leftUnique = nub leftTraces
-    let rightUnique = nub rightTraces
+    let leftUnique = leftTraces
+    let rightUnique = rightTraces
     (leftUnique \\ rightUnique, rightUnique \\ leftUnique)
 
 
 data SMPState = SMP {
-    freeProposers :: Map CallTrace [(Integer, CallTrace, TraceDiff)],
-    engagedProposers :: Map CallTrace [(Integer, CallTrace, TraceDiff)],
-    acceptors :: Map CallTrace [(Integer, CallTrace, TraceDiff)]
+    proposers :: M.Map Integer CallTrace,
+    acceptors :: M.Map Integer CallTrace,
+    preferences :: M.Map (Integer, Integer) (Score, TraceDiff),
+    freeProposers :: M.Map Integer [(Score, Integer)],
+    engagedProposers :: M.Map Integer [(Score, Integer)],
+    engagedAcceptors :: M.Map Integer (Score, Integer),
+    unmatchedProposers :: [Integer]
 }
 
 type SMPMonad = State SMPState
 
 initialSMPState :: SMPState
-initialSMPState = SMP empty empty empty
+initialSMPState = SMP M.empty M.empty M.empty M.empty M.empty M.empty []
 
-addPreference :: CallTrace -> CallTrace -> SMPMonad ()
-addPreference leftTrace rightTrace = do
-    let (score, diff) = alignTraces leftTrace rightTrace
+addProposer :: CallTrace -> SMPMonad ()
+addProposer tr = do
     st <- get
-    let newFP = insertWith appendAndSort leftTrace [(score, rightTrace, diff)] (freeProposers st)
-    let newAcc = insertWith appendAndSort rightTrace [(score, leftTrace, diff)] (acceptors st)
-    put $ st { freeProposers = newFP, acceptors = newAcc }
-    where
-        appendAndSort newPref = reverse . sort . (newPref ++) 
+    let prop = proposers st
+    let id = fromIntegral $ M.size prop
+    put $ st { proposers = M.insert id tr prop }
+
+addAcceptor :: CallTrace -> SMPMonad ()
+addAcceptor tr = do
+    st <- get
+    let acc = acceptors st
+    let id = fromIntegral $ M.size acc
+    put $ st { acceptors = M.insert id tr acc }
+
+fillAllPreferences :: SMPMonad ()
+fillAllPreferences = do
+    st <- get
+    mapM_ (uncurry fillPreference) 
+        [(prop, acc) | prop <- M.keys (proposers st), acc <- M.keys (acceptors st)]
+
+fillPreference :: Integer -> Integer -> SMPMonad ()
+fillPreference propId accId = do
+    st <- get
+    let prop = (proposers st) M.! propId
+    let acc = (acceptors st) M.! accId
+    let newPref@(score, _) = alignTraces prop acc
+    put $ st { preferences = M.insert (propId, accId) newPref (preferences st)}
+
+initFreeProposers :: SMPMonad ()
+initFreeProposers = do
+    st <- get
+    mapM_ initFreeProposer $ M.keys (proposers st)
+
+initFreeProposer :: Integer -> SMPMonad ()
+initFreeProposer prop = do
+    st <- get
+    let prefMap = preferences st
+    let prefs = [(fst (prefMap M.! (prop, acc)), acc) | acc <- M.keys (acceptors st)]
+    let newFp = M.insert prop ((reverse . sort) prefs) (freeProposers st)
+    put $ st { freeProposers = newFp }
+
+initEngagedAcceptors :: SMPMonad ()
+initEngagedAcceptors = do
+    st <- get
+    mapM_ initEngagedAcceptor $ M.keys (acceptors st)
+
+initEngagedAcceptor :: Integer -> SMPMonad ()
+initEngagedAcceptor acc = do
+    st <- get
+    put $ st { engagedAcceptors = M.insert acc (0, -1) (engagedAcceptors st)}
+
+retrieveMatches :: SMPMonad ([TraceDiff], [CallTrace], [CallTrace])
+retrieveMatches = do
+    -- matchedAcceptors
+    return ([], [], [])
+
+findMatches :: SMPMonad ([TraceDiff], [CallTrace], [CallTrace])
+findMatches = do
+    st <- get
+    let fp = freeProposers st
+    if M.null fp 
+        then retrieveMatches 
+        else do
+            let (prop, pref) = M.elemAt 0 fp
+            if null pref || (fst (head pref) == -1) 
+                then do
+                    let newFp = M.delete prop fp
+                    let up = unmatchedProposers st
+                    put $ st { freeProposers = newFp, unmatchedProposers = (prop:up) }
+                    findMatches
+                else do 
+                    let (score, acc) = head pref
+                    let ea = engagedAcceptors st
+                    let (currentScore, currentProposer) = ea M.! acc
+                    if currentScore >= score
+                        then do
+                            let newFp = M.insert prop (tail pref) fp
+                            put $ st { freeProposers = newFp }
+                            findMatches
+                        else do
+                            let ep = engagedProposers st
+                            let currentPref = ep M.! currentProposer
+                            let newFp = M.insert currentProposer (tail currentPref) $ M.delete prop fp
+                            let newEp = M.insert prop pref $ M.delete currentProposer ep
+                            let newEa = M.insert acc (score, prop) ea
+                            put $ st { freeProposers = newFp, engagedProposers = newEp, engagedAcceptors = newEa }
+                            findMatches
+
